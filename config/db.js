8f7c1c74a11c248f93ab8d7d5f128462d3ad4611
ommoco28 <odbayar@mongolcontent.mn>
2023-09-18 10:41:26 +0800

    update ignored files

--- config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-  const connect = await mongoose.connect(process.env.MONGODB_URI);
-  console.log(`mongodb connected : ${connect.connection.host}`);
-};
-
-module.exports = connectDB;
